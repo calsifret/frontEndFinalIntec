30ae83f56f1eaa1705356db640d0ee29acc2a72f
SERGIO GUTTIERREZ <SergioG@cursisten.intecbrussel.intra>
2019-09-23 15:38:56 +0200

    user component create, and working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
