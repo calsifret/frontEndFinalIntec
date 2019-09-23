832af7d7c43794da2ea6588ef08d0fd2bdbe146a
SERGIO GUTTIERREZ <SergioG@cursisten.intecbrussel.intra>
2019-09-23 15:25:03 +0200

    app.module fix, deploy on web is ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [ 
-        BrowserModule,
-        AppRoutingModule
-    ],
-    
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class FeatureModule {}
+export class AppModule { }
