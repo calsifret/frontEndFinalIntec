ed037ef228e6fabe2ecb16e2a440a16fb5feb5c3
SERGIO GUTTIERREZ <SergioG@cursisten.intecbrussel.intra>
2019-09-24 10:12:01 +0200

    table create, button create but not funcional yet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { Route } from '@angular/compiler/src/core';
 import { from } from 'rxjs';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes =[
-    {path:'', redirectTo:'/appComponent', pathMatch: 'full'},
-    {path: 'appComponent', component: AppComponent}
+    {path:'', redirectTo:'/userComponent', pathMatch: 'full'},
+    {path: 'appComponent', component: AppComponent},
+    {path: 'userComponent', component: UserComponent}
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,8 @@ import { UserComponent } from './user/user.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
